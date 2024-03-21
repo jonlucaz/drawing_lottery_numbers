8abcef6160ab1acdbb9a412528f2cac6fe38a44e
Jonathan <jonathanlucastri@gmail.com>
2024-03-21 15:49:33 -0300

    Finish logical program, next step is to make a web application with this logical.

---
+++ Lottery/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+int[] vetor = new int[6];
+Random random = new Random();
+int aleatorio;
+
+for (int c = 0; c < 6; c++)
+{
+    aleatorio = random.Next(1, 61);
+    vetor[c] = aleatorio;
+
+
+
+    while (vetor.Contains(aleatorio))
+    {
+        aleatorio = random.Next(1, 61);
+    }
+
+
+}
+
+Array.Sort(vetor);
+
+foreach (int i in vetor)
+{
+    
+    Console.Write(i + " ");
+}
+
+
